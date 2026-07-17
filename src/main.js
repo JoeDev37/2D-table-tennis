87d124f3e5b9df678dd7c53be5791f16bca2432e
JoeDev37 <alonejoe367@gmail.com>
2026-07-17 18:24:56 +0300

    adding futures

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import Phaser, { Physics } from "phaser";
 import TitleScreen from "./sceanes/TitleScreen";
 import Game from "./sceanes/Game";
 import GameBackground from "./sceanes/GameBackground";
+import GameOver from "./sceanes/GameOverScreen";
 
 const config = {
   width: 1200,
@@ -12,6 +13,7 @@ const config = {
     default: "arcade",
     arcade: {
       gravity: { y: 0 },
+      debug: true,
     },
   },
 };
@@ -21,6 +23,7 @@ const game = new Phaser.Game(config);
 game.scene.add("titleScreen", TitleScreen);
 game.scene.add("game", Game);
 game.scene.add("game-background", GameBackground);
+game.scene.add("game-over", GameOver);
 
 game.scene.start("titleScreen");
 // game.scene.start("game");
